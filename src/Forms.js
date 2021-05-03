09c6602fb946b26d831f98566af1f5ff5a763db3
itaysim7 <itaysim7@gmail.com>
2021-05-03 15:38:57 +0300

    github pages

--- src/Forms.js
+++ src/Forms.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react'
+import Select from 'react-select'
+import countryList from 'react-select-country-list'
 import './Forms.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Det()
  {
+    const [value, setValue] = useState('')
+    const options = useMemo(() => countryList().getData(), [])
+
+    const changeHandler = value => {
+      setValue(value)
+    }
 
     const continueClicked = () => 
     {
@@ -33,22 +41,17 @@ function Det()
                     <h6>Education</h6>
                     <form >
                         <select id="education" name="education">
-                            <option value>-Please select your education-</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
+                            <option value="error">-Please select your education-</option>
+                            <option value="Some High School">Some High School</option>
+                            <option value="High School">High School</option>
+                            <option value="Bachelor's Degree">Bachelor's Degree</option>
+                            <option value="Master's Degree">Master's Degree</option>
+                            <option value="Ph.D. or higher">Ph.D. or higher</option>
                         </select>
                     </form>
                     <h6>Nationality</h6>
-                    <form >
-                        <select id="nationality" name="nationality">
-                            <option value>-Please select your nationality-</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
+                    <form style={{width : '300px', marginLeft : '10px'}}>
+                        <Select placeholder="-Please select your Nationality-" options={options} value={value} onChange={changeHandler} />
                     </form>
                 </div>
             </div>
