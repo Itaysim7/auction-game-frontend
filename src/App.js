eb9e1db8ab706066448eb747d1146b34e809a3fb
itaysim7 <itaysim7@gmail.com>
2021-05-13 18:36:48 +0300

    url

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       API.addParticipant({id: worker})
       .then(resp => console.log(resp))
       .catch(error => console.log(error))
+       window.location.href = `/description/${worker}`;
     }
     else
       alert("Please enter your Worker ID to continue")
